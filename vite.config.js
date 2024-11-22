94eb74991eb2cd583bfa362f4c0d963714d648ba
Yogen <prajapatiyogen1112@gmail.com>
2024-11-22 11:40:23 +0530

    Updates

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/todo-react/', // Replace with your repository name
-});
